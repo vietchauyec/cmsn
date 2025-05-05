5c22f0cd93790b0c89cd7862dda13162dc70af5b
Hải Long <hailong2004ptcnn@gmail.com>
2025-05-05 15:48:08 +0700

    feat: update birthday message and add animated GIF

--- src/App.vue
+++ src/App.vue
@@ -30,22 +30,37 @@ if (days === 0) {
     >
       <h1 class="text-xl font-semibold">{{ message }}</h1>
       <h3 v-if="days !== 0" class="font-writing-italic mb-4">
-        Nhưng thôi cứ đọc thư đi, hì 😎
+        Nhưng thôi cứ đọc thư đi 😎
       </h3>
       <span class="flex flex-col gap-2 text-justify">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          itaque facilis ratione officia voluptatum accusantium tempore nostrum
-          exercitationem iste sed assumenda sint natus, tenetur vitae temporibus
-          labore autem rem aliquid!
+          Hì tui làm thêm cái web lặt vặt chúc mừng sinh nhật em yêu lena 👽
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, fuga
-          animi perferendis praesentium tenetur alias hic eaque nostrum unde
-          doloribus, consectetur ducimus ea porro pariatur ipsum aliquam iusto
-          enim delectus.
+          Lena tuổi mới trước hết là thật mạnh khỏe đã ạ. Sức khỏe tốt còn để
+          đạt được ti tỉ mục tiêu nữa 💪
         </p>
-        <p class="font-writing-italic self-end pr-8">cool signature</p>
+        <p>
+          Hơn nữa là chúc Lena ngày càng xinh đẹp. Xinh rồi mà xinh nữa hê hê.
+          Còn phải tài sắc vẹn toàn nữa nên là chúc Lena học hành làm việc giỏi
+          giang chăm chỉ. Học đâu thuận đó, thi cử gì cũng perfect 🤓
+        </p>
+        <p>
+          Và đặc biệt là chúc Lena thật hạnh phúc ạ. Dù làm gì thì cũng phải cảm
+          thấy hạnh phúc và hài lòng, không vướng bận và không hối hận 🎈
+        </p>
+        <p>
+          Lần nữa happy birthday ạ, hi vọng Lena thích nha hehe. Món quả nhỏ
+          hoan hỉ hoan hỉ. love you 💞
+        </p>
+        <div class="flex flex-col items-center self-end pr-8">
+          <img
+            src="./assets/oia-uia.gif"
+            alt="oiia"
+            class="object-fit h-20 w-20"
+          />
+          <p class="font-writing-italic">long lò</p>
+        </div>
       </span>
     </div>
   </div>
